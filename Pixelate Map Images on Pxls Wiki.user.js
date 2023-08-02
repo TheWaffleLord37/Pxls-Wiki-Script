ae04b4e9c096e537beacc0f109f97fdb2641a437
TheWaffleLord37 <102199627+TheWaffleLord37@users.noreply.github.com>
2023-08-02 10:12:36 -0700

    Add files via upload

---
+++ Pixelate Map Images on Pxls Wiki.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Pixelate Map Images on Pxls Wiki
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Applies pixelated effect to the map images on Pxls Wiki
+// @author       You
+// @match        https://wiki.pxls.space/index.php?title=Map:*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  var style = document.createElement('style');
+  style.innerHTML = `
+    .pixelated {
+      image-rendering: -moz-crisp-edges;
+      image-rendering: -o-crisp-edges;
+      image-rendering: -webkit-optimize-contrast;
+      -ms-interpolation-mode: nearest-neighbor;
+      image-rendering: pixelated;
+    }
+  `;
+  document.head.appendChild(style);
+
+  var element = document.getElementById("map_leaflet_1");
+  if (element) {
+    element.classList.add("pixelated");
+  }
+})();
